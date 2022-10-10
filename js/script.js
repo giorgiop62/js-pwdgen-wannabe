7549306d53dcdace8e8a70cfc87d1f725d6cf4f1
giorgip62 <giorgiop62@gmail.com>
2022-10-10 16:42:27 +0200

    insert info

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+const nomeUtente = prompt("come ti chiami?");
+const cognomeUtente = prompt("qual'è il tuo cognome?");
+const colorePreferito = prompt ("Qual è il tuo colore preferito?");
+
+
+document.getElementById("info").innerHTML += nomeUtente +cognomeUtente +colorePreferito;
